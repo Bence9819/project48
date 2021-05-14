029ac570cfc658c34fd0b4aa361fa808fe9e5ca9
robThot <robthot@gmail.com>
2021-05-14 14:45:42 +0200

    helper.js

---
+++ helper.js
@@ -0,0 +1,20 @@
+
+  var array_A=[1,2,3, 5,6];
+  var array_B=[1,2,3, 8,7];
+
+  var arrayUnionFunction=function  (inputA,inputB) {
+    var result=[];
+    inputA.forEach (function  (element) {
+      if (result.indexOf (element)===-1) {
+        result.push (element);
+      }
+    });
+    inputB.forEach (function  (element) {
+      if (result.indexOf (element)===-1) {
+        result.push (element);
+      }
+    });
+    return result;
+  }
+
+  console.log ( arrayUnionFunction (array_A,array_B) );
