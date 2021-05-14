f0a1643748e72f2c94d88838eb35040eb3e65b3f
robThot <robthot@gmail.com>
2021-05-14 15:07:42 +0200

    helper.js+

--- helper.js
+++ helper.js
@@ -11,6 +11,10 @@
       result.push (element);
     });
     inputB.forEach (function  (element) {
+
+      // tomb indexOf (element) visszajon a tomb elem indexevel, ha letezik az az elem a tombben, -1 el jon vissza, ha nem letezik
+      // az elso iteracioban nem kell vizsgalodni (ha tudjuk, hogy nem redundansak a tombok), mert meg ures a result
+      // csak itt,  a masodik iteracioban vizsgalodunk
       if (result.indexOf (element)===-1) {
         result.push (element);
       }
